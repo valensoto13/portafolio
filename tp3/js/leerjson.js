7235fa110d9f65795b10d7a9dc446bd829364112
valensoto13 <163930492+valensoto13@users.noreply.github.com>
2024-06-06 12:16:28 -0300

    orientar html

--- tp3/js/leerjson.js
+++ tp3/js/leerjson.js
@@ -7,23 +7,23 @@ fetch('js/restaurantes.json')
 .then(data => {
       for (let i=0; i < 7; i++){
        
-         document.querySelector('.resultados').innerHTML +=
-         /*html*/`<article class="resto">
-         <a href="restaurante.html" class="resto"></a>
-         <div class="puntu_resto">${data[i].puntuacion}</div>
-         <div class="imagen-resto">
-             <img src="${data[i].avatar.src}" alt="">
-         </div>
-         <h3><a href="restaurante.html"> ${data[i].name}</a></h3>
-         <br>
-         <p>${data[i].category}<br>
-         ${data[i].horario}
-         </p>
-         <br>
-         <button class="estrellas-icon">
+        document.querySelector('.resultados').innerHTML +=
+        /*html*/`<article class="resto">
+            <a href="restaurante.html" class="resto"></a>
+            <div class="puntu_resto">${data[i].puntuacion}</div>
+            <div class="imagen-resto">
+                <img src="${data[i].avatar.src}" alt="">
+            </div>
+            <h3><a href="restaurante.html"> ${data[i].name}</a></h3>
+            <br>
+            <p>${data[i].category}<br>
+            ${data[i].horario}
+            </p>
+            <br>
+            <button class="estrellas-icon">
              ${data[i].rango}
-             </button>
-     </article>` 
+            </button>
+        </article>` 
        
     }
 })
