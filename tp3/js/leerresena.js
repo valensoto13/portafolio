2c30d2f9c14a111e4fad1b2c84f37189a050e39c
valensoto13 <163930492+valensoto13@users.noreply.github.com>
2024-07-02 16:19:57 -0300

    json

--- tp3/js/leerresena.js
+++ tp3/js/leerresena.js
@@ -1,23 +1,32 @@
-// CXOMILLA INVERTIDA
+let options = document.getElementById('options');
 
-let platos = document.querySelector(".resultados")
+let guardar = (id) => {
+    localStorage.setItem("plato", id);
+};
 
-fetch('js/resena_argentino.json')
-.then((response) => {
-     return response.json()
+fetch('js/resto.json')
+    .then(response => {
+        return response.json();
     })
-.then((plato) => {
-      for (let i=0; i < plato.length; i++){
-        platos.innerHTML +=
-        /*html*/`<article class="resto">
+    .then(data => {
+        data.forEach(e => {
+            if (e.id == localStorage.getItem("resto")) {
+                document.querySelector("#resto-name").innerHTML = e.name;
+            }
+        })
+        data.forEach(e => {
+            if (e.id == localStorage.getItem("resto")) {
+                e.menu.forEach(m => {
+                    options.innerHTML += /*html*/`<article class="resto">
                         <div class="imagen-resto">
-                            <img src="${plato[i].img}">
+                            <img src="${m.img}">
                         </div>
-                        <h2>${plato[i].nombre}</h2>
-                        <h4>${plato[i].plato}</h4>
-                        <p>${plato[i].descripcion}</p>
-                        <div class="puntu_resto"> ${plato[i].calificacion} <i class="fa-solid fa-star"></i></div>
+                        <h2>${m.name}</h2>
+                        <h4>${m.plato}</h4>
+                        <p>${m.desc}</p>
+                        <div class="puntu_resto">${'<span class="icons selected">star</span>'.repeat(e.value.stars)}</div>
                     </article><br>`;
-       
-    }
-})
+                })
+            }
+        })
+    })
