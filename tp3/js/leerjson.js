a990a2d017d8e245a41d392eaeaa9fa0b7f316ac
valensoto13 <163930492+valensoto13@users.noreply.github.com>
2024-07-02 11:36:31 -0300

    json resturantes

--- tp3/js/leerjson.js
+++ tp3/js/leerjson.js
@@ -98,7 +98,7 @@ fetch('js/restaurantes.json')
 //     })
 //   })
 
-let restaurante = document.querySelector(".restaurantes")
+let resto = document.querySelector(".restaurantes")
 
 fetch('js/restaurantes.json')
 .then(response => {
@@ -106,21 +106,21 @@ fetch('js/restaurantes.json')
     })
     .then((data) => {
       data.forEach(element => {
-        restaurante.appendChild=""
+        resto.appendChild=""
     
       });
         // almaceno en la variable restaurante el valor de i del FOR
-        let resto = data[i];
+        let restaurante = data[i];
 
         let cartaRestaurante = document.createElement("div");
-        cartaRestaurante.classList.add("restaurante");
+        cartaRestaurante.classList.add("resto");
 
         // almaceno en la variable cartaRestaurante.id el valor de i del restaurante.truck_id
-        cartaRestaurante.id = resto.truck_id;
+        cartaRestaurante.id = restaurante.truck_id;
 
         cartaRestaurante.innerHTML = /*html*/`
-              <h3>${resto[i].name}</h3>
-              <h4>${resto[i].category}</h4>
+              <h3>${restaurante[i].name}</h3>
+              <h4>${restaurante[i].category}</h4>
               <div class="euro">
                 <div class="plata">
                     <i class="fa-solid fa-euro-sign" style="color: #4bec99;"></i>
@@ -128,17 +128,17 @@ fetch('js/restaurantes.json')
                 <div class="plata">
                     <i class="fa-solid fa-euro-sign" style="color: #4bec99;"></i>
                 </div>
-                <div class="plata">${resto[i].rango}</i>
+                <div class="plata">${restaurante[i].rango}</i>
                 </div>
               </div>
-              <div class="puntu_resto">${resto[i].puntuacion}</div>
+              <div class="puntu_resto">${restaurante[i].puntuacion}</div>
               <div class="precio_resto"></div>
               <br><br><br>
               <div class="icons_resto">
                 <div class="iconos">
                     <div class="icon">
                         <i class="fa-regular fa-clock"></i>
-                        <h3>${resto[i].horario}</h3>
+                        <h3>${restaurante[i].horario}</h3>
                     </div>
                     <div class="icon">
                         <i class="fa-solid fa-location-dot"></i>
@@ -151,11 +151,11 @@ fetch('js/restaurantes.json')
                 </div>
               </div>
               <div class="descri">
-                <p>${resto[i].bio}</p>
+                <p>${restaurante[i].bio}</p>
               </div>` 
 
               cartaRestaurante.addEventListener("click", function () {
-                let idRestauranteSeleccionado = resto.truck_id;
+                let idRestauranteSeleccionado = restaurante.truck_id;
                 //almaceno en CACHÉ la variable indiceCache el valor de idRestauranteSeleccionado
                 localStorage.setItem("indiceCache", idRestauranteSeleccionado);
                 console.log(idRestauranteSeleccionado);
