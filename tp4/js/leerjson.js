ae6c9d39a10d06ca00da52b3616fede5362d9ca1
valensoto13 <163930492+valensoto13@users.noreply.github.com>
2024-07-31 12:55:35 -0300

    js

--- tp4/js/leerjson.js
+++ tp4/js/leerjson.js
@@ -16,7 +16,11 @@ fetch('js/viajes.json')
 .then(data => {
     data.forEach(e => {
         document.querySelector('.lugares').innerHTML +=
-        /*html*/`` 
+        /*html*/`            <img class="control prev" src="img/img1.png" alt="">
+            <article class="lugar">
+                <img src="${e.imagen}" alt="">
+                </article>
+            <img class="control next" src="img/img2.png" alt="">` 
     });
 })
 
