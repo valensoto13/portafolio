caf5db6074f7e37679006b0e3135d05eab913aa0
valensoto13 <163930492+valensoto13@users.noreply.github.com>
2024-07-31 12:29:12 -0300

    json menu

--- tp4/js/leerjson.js
+++ tp4/js/leerjson.js
@@ -1,25 +1,31 @@
-// let options = document.getElementById('options');
+let options = document.getElementById('options');
 
-// let guardar = (id) => {
-//     localStorage.setItem("viaje", id);
-// };
+let guardar = (id) => {
+    localStorage.setItem("viaje", id);
+};
 
-// fetch('js/viajes.json')
-// .then(response => {
-//      return response.json()
-//     })
-// .then(data => {
-//     data.forEach(e => {
-//         document.querySelector('.lugares').innerHTML +=
-//         /*html*/`<article class="lugar">
-//                 <div class="imagen">
-//                     <img src="${e.imagen}" alt="">
-//                 </div>
-//             <h3><a id=${e.id} href="lugares.html" onclick=guardar(id)>${e.lugar}</a></h3>
-//             <h4>$${e.precio}</h4>
-//         </article>` 
-//     });
-// })
+fetch('js/viajes.json')
+.then(response => {
+     return response.json()
+    })
+.then(data => {
+    data.forEach(e => {
+        document.querySelector('.lugares').innerHTML +=
+        /*html*/`<article class="lugar">
+                    <div class="imagen">
+                        <section class="splide p-5" aria-label="Splide Basic HTML Example">
+                            <div class="splide__track">
+                                  <ul class="splide__list">
+                                      <li class="splide__slide"><h2 class="text-center"><img src="${e.imagen}" alt="placeholden image"></h2></li>
+                                  </ul>
+                            </div>
+                        </section>
+                    </div>
+                <h3><a id=${e.id} href="lugares.html" onclick=guardar(id)>${e.lugar}</a></h3>
+                <h4>$${e.precio}</h4>
+            </article>` 
+    });
+})
 
 document.addEventListener( 'DOMContentLoaded', function() {
     var splide = new Splide( '.splide' );
