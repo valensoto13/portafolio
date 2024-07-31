29fab2af82a697766cea83d128670c0d2c432218
valensoto13 <163930492+valensoto13@users.noreply.github.com>
2024-07-31 12:56:45 -0300

    html

--- tp4/js/leerjson.js
+++ tp4/js/leerjson.js
@@ -4,24 +4,24 @@ let guardar = (id) => {
     localStorage.setItem("viaje", id);
 };
 
-document.addEventListener( 'DOMContentLoaded', function() {
-    var splide = new Splide( '.splide' );
+document.addEventListener('DOMContentLoaded', function () {
+    var splide = new Splide('.splide');
     splide.mount();
-  } );
+});
 
 fetch('js/viajes.json')
-.then(response => {
-     return response.json()
+    .then(response => {
+        return response.json()
     })
-.then(data => {
-    data.forEach(e => {
-        document.querySelector('.lugares').innerHTML +=
+    .then(data => {
+        data.forEach(e => {
+            document.querySelector('.lugares').innerHTML +=
         /*html*/`            <img class="control prev" src="img/img1.png" alt="">
             <article class="lugar">
                 <img src="${e.imagen}" alt="">
                 </article>
-            <img class="control next" src="img/img2.png" alt="">` 
-    });
-})
+            <img class="control next" src="img/img2.png" alt="">`
+        });
+    })
 
 
