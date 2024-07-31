8995f49e46f1e7e2df371afa828efd372f9ab034
valensoto13 <163930492+valensoto13@users.noreply.github.com>
2024-07-31 12:58:38 -0300

    js

--- tp4/js/leerjson.js
+++ tp4/js/leerjson.js
@@ -4,10 +4,6 @@ let guardar = (id) => {
     localStorage.setItem("viaje", id);
 };
 
-document.addEventListener('DOMContentLoaded', function () {
-    var splide = new Splide('.splide');
-    splide.mount();
-});
 
 fetch('js/viajes.json')
     .then(response => {
