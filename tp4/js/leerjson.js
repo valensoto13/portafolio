0c823e5079c14048b1e76192b3d3079f5ed986d0
valensoto13 <163930492+valensoto13@users.noreply.github.com>
2024-07-31 12:54:44 -0300

    css y html

--- tp4/js/leerjson.js
+++ tp4/js/leerjson.js
@@ -16,19 +16,7 @@ fetch('js/viajes.json')
 .then(data => {
     data.forEach(e => {
         document.querySelector('.lugares').innerHTML +=
-        /*html*/`<article class="lugar">
-                    <div class="imagen">
-                        <section class="splide p-5" aria-label="Splide Basic HTML Example">
-                            <div class="splide__track">
-                                  <ul class="splide__list">
-                                      <li class="splide__slide"><h2 class="text-center"><img src="${e.imagen}" alt="placeholden image"></h2></li>
-                                  </ul>
-                            </div>
-                        </section>
-                    </div>
-                <h3><a id=${e.id} href="lugares.html" onclick=guardar(id)>${e.lugar}</a></h3>
-                <h4>$${e.precio}</h4>
-            </article>` 
+        /*html*/`` 
     });
 })
 
