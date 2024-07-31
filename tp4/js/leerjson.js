877d22bbea34e38ff7d7c52a956e8846178a1c36
valensoto13 <163930492+valensoto13@users.noreply.github.com>
2024-07-31 12:14:42 -0300

    splide imagen

--- tp4/js/leerjson.js
+++ tp4/js/leerjson.js
@@ -1,25 +1,25 @@
-let options = document.getElementById('options');
+// let options = document.getElementById('options');
 
-let guardar = (id) => {
-    localStorage.setItem("viaje", id);
-};
+// let guardar = (id) => {
+//     localStorage.setItem("viaje", id);
+// };
 
-fetch('js/viajes.json')
-.then(response => {
-     return response.json()
-    })
-.then(data => {
-    data.forEach(e => {
-        document.querySelector('.lugares').innerHTML +=
-        /*html*/`<article class="lugar">
-                <div class="imagen">
-                    <img src="${e.imagen}" alt="">
-                </div>
-            <h3><a id=${e.id} href="lugares.html" onclick=guardar(id)>${e.lugar}</a></h3>
-            <h4>$${e.precio}</h4>
-        </article>` 
-    });
-})
+// fetch('js/viajes.json')
+// .then(response => {
+//      return response.json()
+//     })
+// .then(data => {
+//     data.forEach(e => {
+//         document.querySelector('.lugares').innerHTML +=
+//         /*html*/`<article class="lugar">
+//                 <div class="imagen">
+//                     <img src="${e.imagen}" alt="">
+//                 </div>
+//             <h3><a id=${e.id} href="lugares.html" onclick=guardar(id)>${e.lugar}</a></h3>
+//             <h4>$${e.precio}</h4>
+//         </article>` 
+//     });
+// })
 
 document.addEventListener('DOMContentLoaded', function () {
     new Splide('.splide', {
