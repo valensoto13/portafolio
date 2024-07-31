0b6733dd95d98a6efd21a7de3612ba68c6fb8c8d
valensoto13 <163930492+valensoto13@users.noreply.github.com>
2024-07-31 10:26:53 -0300

    tp3 resto js

--- tp3/js/leerresto.js
+++ tp3/js/leerresto.js
@@ -9,14 +9,20 @@ fetch('js/resto.json')
         return response.json();
     })
     .then(data => {
-        data.forEach(e => {
-            if (e.id == localStorage.getItem("resto")) {
-                document.querySelector("#resto-name").innerHTML = e.name;
-            }
-        })
-        data.forEach(e => {
-            if (e.id == localStorage.getItem("resto")) {
-                e.menu.forEach(m => {
+        let restoId = localStorage.getItem("resto");
+        
+        if (!restoId) {
+            console.error("No hay un 'resto' en localStorage");
+            return;
+        }
+
+        let resto = data.find(e => e.id == restoId);
+
+        if (resto) {
+            document.querySelector("#resto-name").innerHTML = resto.name;
+            options.innerHTML = ""; // Limpiar el contenedor antes de agregar nuevo contenido
+
+            resto.menu.forEach(m => {
                 options.innerHTML += `
                 <h3 id="${m.id}">${m.name}</h3>
                 <h4>${m.ubic}</h4>
@@ -27,9 +33,9 @@ fetch('js/resto.json')
                     <div class="plata">
                         <i class="fa-solid fa-euro-sign" style="color: #4bec99;"></i>
                     </div>
-                    <div class="plata">${'<span class="icons">euro_symbol</span>'.repeat(3-e.value.cost)}</div>
+                    <div class="plata">${'<span class="icons">euro_symbol</span>'.repeat(3 - m.cost)}</div>
                 </div>
-                <div class="puntu_resto">${'<span class="icons selected">star</span>'.repeat(e.value.stars)}</div>
+                <div class="puntu_resto">${'<span class="icons selected">star</span>'.repeat(resto.stars)}</div>
                 <div class="precio_resto"></div>
                 <br><br><br>
                 <div class="icons_resto">
@@ -40,7 +46,7 @@ fetch('js/resto.json')
                         </div>
                         <div class="icon">
                             <i class="fa-solid fa-location-dot"></i>
-                            <h3>${m.dist}/h3>
+                            <h3>${m.dist}</h3>
                         </div>
                         <div class="icon">
                             <i class="fa-solid fa-bell-concierge"></i>
@@ -49,8 +55,9 @@ fetch('js/resto.json')
                     </div>
                 </div>
                 <div class="descri"><p>${m.bio}</p></div>`;
-            })
+            });
+        } else {
+            console.error("No se encontró el restaurante con id: " + restoId);
         }
     })
-})
-
+    .catch(error => console.error('Error al cargar el JSON:', error));
