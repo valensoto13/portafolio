b4a117f78e144bc83552438eb761c6419790bd32
valensoto13 <valensoto1305@gmail.com>
2024-08-26 17:53:00 -0300

    todo el tp4

--- tp4/js/leerjson.js
+++ tp4/js/leerjson.js
@@ -1,22 +1,66 @@
-let options = document.getElementById('options');
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('js/viajes.json')
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById('options');
+            container.innerHTML = ''; // Limpia el contenedor antes de agregar los datos
 
-let guardar = (id) => {
-    localStorage.setItem("viaje", id);
-};
+            data.forEach(e => {
+                const article = document.createElement('article');
+                article.classList.add('lugar');
+                
+                article.innerHTML = `
+                    <div class="imagen">
+                        <img src="${e.imagen}" alt="">
+                    </div>
+                    <h3><a href="lugares.html" data-id="${e.id}">${e.lugar}</a></h3>
+                    <h4>$${e.precio}</h4>
+                `;
+                
+                container.appendChild(article);
+            });
+
+            // Añadir el manejador de eventos para los enlaces después de cargarlos
+            document.querySelectorAll('a[data-id]').forEach(link => {
+                link.addEventListener('click', function() {
+                    const id = this.getAttribute('data-id');
+                    localStorage.setItem('selectedPlaceId', id);
+                });
+            });
+        })
+        .catch(error => console.error('Error fetching the JSON:', error));
+});
 
 
-fetch('js/viajes.json')
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        data.forEach(e => {
-            document.querySelector('.contenedor').innerHTML +=
-        /*html*/`
-            <article class="lugar">
-                <img src="${e.imagen}" alt="">
-            </article>`
-        });
-    })
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('js/viajes.json')
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById('op');
+            container.innerHTML = ''; // Limpia el contenedor antes de agregar los datos
 
+            data.forEach(e => {
+                const article = document.createElement('article');
+                article.classList.add('aventu');
+                
+                article.innerHTML = `
+                    <div class="imagen">
+                        <img src="${e.aventura.imagen}" alt="">
+                    </div>
+                    <h3><a href="aventura.html" data-id="${e.id}">${e.aventura.aventura}</a></h3>
+                    <h4>$${e.precio}</h4>
+                `;
+                
+                container.appendChild(article);
+            });
 
+            // Añadir el manejador de eventos para los enlaces después de cargarlos
+            document.querySelectorAll('a[data-id]').forEach(link => {
+                link.addEventListener('click', function() {
+                    const id = this.getAttribute('data-id');
+                    localStorage.setItem('selectedPlaceId', id);
+                });
+            });
+        })
+        .catch(error => console.error('Error fetching the JSON:', error));
+});
--- tp4/js/leerlugar.js
+++ tp4/js/leerlugar.js
@@ -1,60 +1,122 @@
-let options = document.getElementById('options');
+document.addEventListener('DOMContentLoaded', function() {
+    const id = localStorage.getItem('selectedPlaceId');
 
-let guardar = (id) => {
-    localStorage.setItem("plato", id);
-};
-
-fetch('js/resto.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-
-        let restoId = localStorage.getItem("resto");
-        console.log('Resto ID:', restoId);  // Verifica el valor recuperado del localStorage
-
-        data.forEach(e => {
-            if (e.id == restoId) {
-                document.querySelector("#resto-name").innerHTML = e.lugar;
-                options.innerHTML = '';  // Limpia el contenido existente
-                e.menu.forEach(m => {
-                    options.innerHTML += `
-                    <img src="${m.imagen}" alt="">
-                    <h3 id="${m.id}">${m.lugar}</h3>
-                    <h4>Overview</h4>
-                    <br><br><br>
-                    <div class="icons_resto">
-                        <div class="iconos">
-                            <div class="icon">
-                                <div class="lado">
-                                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
-                                    <h2>${m.ranking}</h2>
+    if (id) {
+        fetch('js/viajes.json')
+            .then(response => response.json())
+            .then(data => {
+                const item = data.find(m => m.id === id);
+                if (item) {
+                    document.querySelector('.restaurantes').innerHTML = `
+                        <div class="imagen">
+                            <img src="${item.imagen}" alt="">
+                        </div>
+                        <h3 id="${item.id}">${item.lugar}</h3>
+                        <h4>Overview</h4>
+                        <br><br><br>
+                        <div class="icons_resto">
+                            <div class="iconos">
+                                <div class="icon">
+                                    <div class="lado">
+                                        <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                        <h2>${item.ranking}</h2>
+                                    </div>
+                                    <h5>Rating</h5>
                                 </div>
-                                <h5>Rating</h5>
-                            </div>
-                            <div class="icon">
-                                <div class="lado">
-                                    <i class="fa-solid fa-cloud" style="color: #74C0FC;"></i>
-                                    <h2>${m.grados}°C</h2>
+                                <div class="icon">
+                                    <div class="lado">
+                                        <i class="fa-solid fa-cloud" style="color: #74C0FC;"></i>
+                                        <h2>${item.grados}°C</h2>
+                                    </div>
+                                    <h5>Temperature</h5>
                                 </div>
-                                <h5>Duration</h5>
-                            </div>
-                            <div class="icon">
-                                <div class="lado">
-                                    <i class="fa-regular fa-clock"></i>
-                                    <h2>${m.duracion} hrs</h2>
+                                <div class="icon">
+                                    <div class="lado">
+                                        <i class="fa-regular fa-clock"></i>
+                                        <h2>${item.duracion} hrs</h2>
+                                    </div>
+                                    <h5>Duration</h5>
                                 </div>
-                                <h5>Duration</h5>
                             </div>
                         </div>
-                    </div>
-                    <div class="descri">
-                        <p>${m.descripcion}</p>
-                    </div>`;
-                });
-            }
-        });
-    })
+                        <div class="descri">
+                            <p>${item.descripcion}</p>
+                        </div>
+                        <div class="final">
+                        <br>
+                            <p>Total price: </p>
+                            <h1>$${item.precio}</h1>
+                        </div>
+                    `;
+                } else {
+                    document.querySelector('.restaurantes').innerHTML = 'No se encontró el lugar.';
+                }
+            })
+            .catch(error => console.error('Error fetching the JSON:', error));
+    } else {
+        document.querySelector('.restaurantes').innerHTML = 'No se ha seleccionado ningún lugar.';
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const id = localStorage.getItem('selectedPlaceId');
+
+    if (id) {
+        fetch('js/viajes.json')
+            .then(response => response.json())
+            .then(data => {
+                const item = data.find(m => m.id === id);
+                if (item) {
+                    document.querySelector('.aventuras').innerHTML = `
+                        <div class="imagen">
+                            <img src="${item.aventura.imagen}" alt="">
+                        </div>
+                        <h3 id="${item.aventura.id}">${item.aventura.aventura}</h3>
+                        <h4>Overview</h4>
+                        <br><br><br>
+                        <div class="icons_resto">
+                            <div class="iconos">
+                                <div class="icon">
+                                    <div class="lado">
+                                        <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                        <h2>${item.ranking}</h2>
+                                    </div>
+                                    <h5>Rating</h5>
+                                </div>
+                                <div class="icon">
+                                    <div class="lado">
+                                        <i class="fa-solid fa-cloud" style="color: #74C0FC;"></i>
+                                        <h2>${item.aventura.dificultad}</h2>
+                                    </div>
+                                    <h5>Dificultad</h5>
+                                </div>
+                                <div class="icon">
+                                    <div class="lado">
+                                        <i class="fa-regular fa-clock"></i>
+                                        <h2>${item.aventura.duracion} hrs</h2>
+                                    </div>
+                                    <h5>Duration</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="descri">
+                            <p>${item.aventura.descripcion}</p>
+                        </div>
+                        <div class="final">
+                            <br><br>
+                            <p>Total price: </p>
+                            <h1>$${item.aventura.precio}</h1>
+                        </div>
+                    `;
+                } else {
+                    document.querySelector('.aventuras').innerHTML = 'No se encontró el lugar.';
+                }
+            })
+            .catch(error => console.error('Error fetching the JSON:', error));
+    } else {
+        document.querySelector('.aventuras').innerHTML = 'No se ha seleccionado ningún lugar.';
+    }
+});
+
+
