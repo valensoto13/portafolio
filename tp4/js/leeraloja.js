976e2023947baee5a5eb9a6cfc44bbb3ae8fa629
Valentina <valensoto1305@gmail.com>
2024-08-27 22:41:05 -0300

    aloja.html

---
+++ tp4/js/leeraloja.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const id = localStorage.getItem('selectedPlaceId');
+
+    if (id) {
+        fetch('js/alojamiento.json')
+            .then(response => response.json())
+            .then(data => {
+                const item = data.find(m => m.id === id);
+                if (item) {
+                    document.querySelector('.alojamiento').innerHTML = `
+                        <div class="imagen">
+                            <img src="${item.imagen}" alt="">
+                        </div>
+                        <h3 id="${item.id}">${item.lugar}</h3>
+                        <h4>Overview</h4>
+                        <br><br><br>
+                        <div class="icons_resto">
+                            <div class="iconos">
+                                <div class="icon">
+                                    <div class="lado">
+                                        <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                        <h2>${item.ranking}</h2>
+                                    </div>
+                                    <h5>Rating</h5>
+                                </div>
+                                <div class="icon">
+                                    <div class="lado">
+                                        <i class="fa-solid fa-cloud" style="color: #74C0FC;"></i>
+                                        <h2>${item.grados}°C</h2>
+                                    </div>
+                                    <h5>Temperature</h5>
+                                </div>
+                                <div class="icon">
+                                    <div class="lado">
+                                        <i class="fa-solid fa-location-dot"></i>
+                                        <h2 class="distancia">${alojamiento.distancia}</h2>
+                                    </div>
+                                    <h5>Distancia</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="descri">
+                            <p>${item.descripcion}</p>
+                        </div>
+                        <div class="final">
+                        <br>
+                            <p>Total price: </p>
+                            <h1>$${item.precio}</h1>
+                        </div>
+                    `;
+                } else {
+                    document.querySelector('.alojamiento').innerHTML = 'No se encontró el lugar.';
+                }
+            })
+            .catch(error => console.error('Error fetching the JSON:', error));
+    } else {
+        document.querySelector('.alojamiento').innerHTML = 'No se ha seleccionado ningún lugar.';
+    }
+});
